f78e0da55a11cb28f2eb3a7b4d96df121dfbb2ff
Erik Thiem <erikthiem@gmail.com>
2016-09-24 12:50:39 -0500

    separated app and image files, added gitignore

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-import sys
-import cv2
-import numpy as np
+from flask import Flask
+app = Flask(__name__)
 
-class MyImage:
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-	def __init__(self, path):
-		self.image = cv2.imread(path)
-
-	def displayImage(self):
-		cv2.imshow('image', self.image)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
-
-	def applyGray(self):
-		self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-
-
-path = sys.argv[1]
-
-image = MyImage(path)
-image.applyGray()
-image.displayImage()
+if __name__ == "__main__":
+    app.run()
