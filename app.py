49520e320eb61bfe67d180f7cda49c7208772d4f
Erik Thiem <erikthiem@gmail.com>
2016-09-24 21:33:39 -0500

    started upload method

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def homepage():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files['image_file']
+    filename = secure_filename(file.filename)
+    return filename
 
 if __name__ == "__main__":
     app.run()
